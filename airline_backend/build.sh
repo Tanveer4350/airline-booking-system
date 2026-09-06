37d2dd44ae1b4c6e564b5c97d7860fd9fa9a8bc8
Tanveer4350 <tanveersingh4350@gmail.com>
2026-09-06 12:20:14 +0530

    Automate superuser creation and flight data seeding in build script

--- airline_backend/build.sh
+++ airline_backend/build.sh
@@ -5,3 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py create_initial_superuser
+python manage.py seed_data --days 7
